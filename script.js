5d913ab0f299ea566f6a4925641ff46f1e99284c
Hunter Yavitz <8253185+hunteryavitz@users.noreply.github.com>
2021-02-06 10:21:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const jokeElement = document.getElementById('joke')
+const jokeButton = document.getElementById('jokeBtn')
+
+jokeButton.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        }
+    }
+
+    const resp = await fetch('https://icanhazdadjoke.com', config)
+    const data = await resp.json()
+
+    jokeElement.innerHTML = data.joke    
+}
